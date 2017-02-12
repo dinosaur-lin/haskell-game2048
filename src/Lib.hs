1c1c06f1db3ff2f72a154e2342fa8dc1084853f4
dinosaur-lin <yunfeng.lin04@gmail.com>
2017-02-11 21:32:31 -0500

    make it more functional

--- src/Lib.hs
+++ src/Lib.hs
@@ -13,8 +13,12 @@ convertVector2Str :: Int -> (V.Vector Int) -> [Char]
 convertVector2Str len vec = let vecStr = fmap (\x -> convertNum2Str x len) vec in
                             foldl (\x y -> x ++ y) "" vecStr
 
+convertMatrix2Str :: Int -> (V.Vector (V.Vector Int)) -> [Char]
+convertMatrix2Str len mat = let mstr = fmap (\x -> convertVector2Str len x) mat in
+                              foldl (\x y -> x ++ "\n" ++ y) "" mstr
+
 printMatrix :: Int -> (V.Vector (V.Vector Int)) -> IO ()
-printMatrix len vec = mapM_ putStrLn (fmap (convertVector2Str len) vec)
+printMatrix len mat = putStrLn (convertMatrix2Str len mat)
 
 start :: IO ()
 start = printMatrix 5 (V.generate 4 (\n -> V.replicate 4 n))
